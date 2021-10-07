60c4c982e8860d1ca71f5c995e202355d79ca634
Revathi Yarlagadda <revathi.yarlagadda@outlook.com>
2021-10-07 10:59:21 +0100

    test commit

---
+++ $webresult = Invoke-WebRequest -UseBasic.ps1
@@ -0,0 +1,22 @@
+$webresult = Invoke-WebRequest -UseBasicParsing -Uri google.com1 -ErrorAction SilentlyContinue
+
+if ($webresult.statuscode -ne 200 ) {
+    Write-Host $Error
+} else {
+    Write-Host $webresult.Content
+}
+
+
+function get-revathi {
+ param (
+     $uri
+ )
+
+    $webresult = Invoke-WebRequest -UseBasicParsing -Uri $uri -ErrorAction SilentlyContinue
+
+if ($webresult.statuscode -ne 200 ) {
+    Write-Host $Error
+} else {
+    Write-Host $webresult.StatusDescription
+}
+}
